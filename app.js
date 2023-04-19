576b5248927e6f7341bde15968d601694214c748
saskyapimenta <saskya.pimenta@hotmail.com>
2023-04-19 15:02:25 -0300

    voltando para o estado do paulo

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
 console.log("Rodando osistema de gerencimento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
-//mais um console de teste:
-console.log("O Paulo fez esse commit sem você ver na aula!")
-console.log("Enviando para o Github")
-
